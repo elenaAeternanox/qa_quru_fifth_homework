820dc26020dae8562e9846e82d6333cb637ef3c5
elenaAeternanox <antares87@mail.ru>
2021-11-07 12:26:49 +0100

    Fix day in calendar

--- src/test/java/fifthHomework/pages/components/CalendarComponent.java
+++ src/test/java/fifthHomework/pages/components/CalendarComponent.java
@@ -3,8 +3,8 @@ package fifthHomework.pages.components;
 import com.codeborne.selenide.SelenideElement;
 
 import static com.codeborne.selenide.Selectors.byClassName;
-import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.$x;
 
 public class CalendarComponent {
 
@@ -12,9 +12,10 @@ public class CalendarComponent {
 
     public void setDate(String day, String month, String year) {
         dateInput.click();
+
         $(byClassName("react-datepicker__year-select")).selectOption(year);
         $(byClassName("react-datepicker__month-select")).selectOption(month);
-        $(byText(day)).click();
+        $x("//div[text()='" + day + "' and (contains(@aria-label,'" + month + "'))]").click();
     }
 
 }
